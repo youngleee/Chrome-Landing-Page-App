8277884631f2565dde18707681bc932ab22282b6
Jongyoung <youngleeee@outlook.com>
2024-08-08 15:44:14 +0200

    login button and form added

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+// OR
+
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("login-form button");
+// Either works but the 2nd one is shorter and cleaner
+
