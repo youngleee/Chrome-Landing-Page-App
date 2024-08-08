4c39105aafa698a7856b2b9ce6f300e6930f8ed9
Jongyoung <youngleeee@outlook.com>
2024-08-08 16:02:17 +0200

    added if statements to check validity of username

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const loginButton = loginForm.querySelector("button");
 // Either works but the 2nd one is shorter and cleaner
 
 function handleLoginButtonClick() {
-    console.log("click");
+    const username = loginInput.value;
+    if (username ==='') {
+        alert("Please type in your name!");
+    } else if (username.length > 14) {
+        alert("your name is too long");
+    }
 }
 
 loginButton.addEventListener("click",handleLoginButtonClick);
