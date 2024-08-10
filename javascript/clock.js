543548aac296d3a6fe0ab288f2b6dad966df534b
Young Lee <youngleeee@outlook.com>
2024-08-10 14:38:59 +0200

    finished  with clock but unstyled

--- javascript/clock.js
+++ javascript/clock.js
@@ -2,12 +2,13 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 // setInterval has 2 arguments: function, time interval
 // setInterval(sayHello, 5000);
 
+getClock();
 setInterval(getClock, 1000);
 
 //setTimeout has 2 arguments: function, after how many milliseconds.
